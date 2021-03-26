b2d7e9095ef5bd0cd084c1b367467310808ab39e
agouy <contact@agouy.com>
2021-03-26 09:37:06 +0000

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import SAUCIE
+import tensorflow as tf
 import pandas as pd
-import numpy as np
 
-def run_py_script(df):
-  df = df[[".ci", ".ri", ".y"]]
-  df.groupby([".ci", ".ri"]).aggregate(np.median)
-  return df
+def run_py_script(data):
+  tf.reset_default_graph()
+  saucie = SAUCIE.SAUCIE(data.shape[1])
+  loadtrain = SAUCIE.Loader(data, shuffle=True)
+  saucie.train(loadtrain, steps=1000)
+  loadeval = SAUCIE.Loader(data, shuffle=False)
+  embedding = saucie.get_embedding(loadeval)
+  number_of_clusters, clusters = saucie.get_clusters(loadeval)
+  df_out = pd.DataFrame(data=embedding, columns=["SAUCIE1", "SAUCIE2"])
+  df_out["clusters"] = clusters
+  #reconstruction = saucie.get_reconstruction(loadeval)
+  return df_out
--- workspace.R
+++ workspace.R
@@ -8,11 +8,29 @@ options("tercen.stepId"     = "52110340-a9c9-49fd-ba1e-a9b0cc4639b4")
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
+# reticulate::py_install(c("tensorflow==1.14"))
+# , "numpy", "pandas", "scikit-learn", "matplotlib"))
+# # "fcsparser", "fcswrite" missing
+# py_install("pandas")
+# py_install("tensorflow==1.4.0")
+# py_install("scikit-learn")
+# py_install("numpy==1.13.3", method = "conda")
+
+# import("SAUCIE")
+renv::init()
+renv::use_python()
+reticulate:::py_install_method_detect()
+reticulate::py_install("pandas")
+reticulate::py_install("sklearn")
+reticulate::py_install("tensorflow==1.4.0")
+renv::snapshot()
+
 reticulate::source_python("main.py")
 
 df <- (ctx <- tercenCtx()) %>%
-  select() %>% run_py_script()
+  as.matrix() %>% t %>% run_py_script()
 
-df %>%
+df %>% as_tibble %>%
+  mutate(.ci = seq_len(nrow(df)) - 1) %>%
   ctx$addNamespace() %>%
   ctx$save()
